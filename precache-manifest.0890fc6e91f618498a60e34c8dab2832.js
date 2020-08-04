d6dd2a83d57a2feee623c12980912edceb8f0959
Meenakshi Sundaram. C <41613443+GamingBug@users.noreply.github.com>
2020-08-04 19:39:26 +0530

    Updates

---
+++ precache-manifest.0890fc6e91f618498a60e34c8dab2832.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7cdee558cf407c98ad445f74b340170",
+    "url": "/Covid-19-Live-Stats/index.html"
+  },
+  {
+    "revision": "c1d011a05c132227001f",
+    "url": "/Covid-19-Live-Stats/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f58ae2fd6d3ac14e6517",
+    "url": "/Covid-19-Live-Stats/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c1d011a05c132227001f",
+    "url": "/Covid-19-Live-Stats/static/js/2.7b1e48f0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Covid-19-Live-Stats/static/js/2.7b1e48f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58ae2fd6d3ac14e6517",
+    "url": "/Covid-19-Live-Stats/static/js/main.f7d97524.chunk.js"
+  },
+  {
+    "revision": "4812ba1758becc80f88d",
+    "url": "/Covid-19-Live-Stats/static/js/runtime-main.6a73088d.js"
+  }
+]);
